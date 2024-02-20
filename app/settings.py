31ab624521d7e6319e6448b1cd7e5bb6aeb8391e
Jaime Benalcazar <jaimeb_2006@hotmail.com>
2024-02-19 22:15:47 -0500

    Agregar PostGres

--- app/settings.py
+++ app/settings.py
@@ -99,6 +99,17 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'inbalnor',
+#         'USER': 'inbalnor_sismode',
+#         'PASSWORD': 'InbalnoSismode0117',
+#         'HOST': 'localhost',  
+#         'PORT': '5432',
+#     }
+# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
